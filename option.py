af7a99a445cd74371ac0849b926c5153603fd539
Kilinstore <103324979+Kilin-Store@users.noreply.github.com>
2022-04-10 12:02:49 +0800

    Create option.py

---
+++ option.py
@@ -0,0 +1,11 @@
+import sys, os
+
+N = '\x1b[0m'    # WARNA MATI
+B = '\x1b[1;94m' # BIRU
+M = '\x1b[1;91m' # MERAH
+
+if __name__ == '__main__':
+    os.system("git pull")
+    os.system("pip install rich")
+    os.system("
+    os.system("clear")
